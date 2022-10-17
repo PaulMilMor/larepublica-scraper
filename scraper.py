fec9c01261f8ea6a8a9447ad986a60ba4e34256c
Paúl Millanes <pauljmm99@gmail.com>
2022-10-16 23:01:21 -0700

    Writting each new article into a file

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import requests
 import lxml.html as html
+import os
+import datetime
 
 
 HOME_URL = 'https://www.larepublica.co/'
@@ -10,6 +12,38 @@ XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/descendant-or-self::text()'
 
 
+def parse_news(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            new = response.content.decode('utf-8')
+            parsed = html.fromstring(new)
+
+            try: 
+                title = parsed.xpath(XPATH_TITLE)[0]
+                print(title)
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                title = title.replace('\"','')
+                title = title.replace('\n                        ','')
+                title = title.replace('\n                    ', '')
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError:
+                return
+
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                f.write(summary)
+                for p in body:
+                    f.write(p)
+
+                    f.write('\n')
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
+
+
 def parse_home():
     try:
         response = requests.get(HOME_URL)
@@ -17,7 +51,14 @@ def parse_home():
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             links_to_news = parsed.xpath(XPATH_LINK_TO_ARTICLE)
-            print(links_to_news)
+            # print(links_to_news)
+
+            today = datetime.date.today().strftime('%d-%m-%Y')
+            if not os.path.isdir(today):
+                os.mkdir(today)
+            
+            for link in links_to_news:
+                parse_news(link, today)
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
