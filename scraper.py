6a69c883400cba838fb0126c777bb5a0b04a8ed2
Paúl Millanes <pauljmm99@gmail.com>
2022-10-16 22:34:56 -0700

    Add scraper to get news links

---
+++ scraper.py
@@ -0,0 +1,32 @@
+import requests
+import lxml.html as html
+
+
+HOME_URL = 'https://www.larepublica.co/'
+
+XPATH_LINK_TO_ARTICLE = '//text-fill/a/@href'
+XPATH_TITLE = '//div[@class="mb-auto"]/h2/span/text()'
+XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
+XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/descendant-or-self::text()'
+
+
+def parse_home():
+    try:
+        response = requests.get(HOME_URL)
+        if response.status_code == 200:
+            home = response.content.decode('utf-8')
+            parsed = html.fromstring(home)
+            links_to_news = parsed.xpath(XPATH_LINK_TO_ARTICLE)
+            print(links_to_news)
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
+
+
+def run():
+    parse_home()
+
+    
+if __name__ == '__main__':
+    run()
